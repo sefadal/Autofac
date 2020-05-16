fe00cff581430cb882aa22c08cf34a02b7365f01
sefadal <sefadal@hotmail.com>
2020-05-16 16:18:58 +0300

    Add project files.

---
+++ Entities/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entites;
+
+namespace Entities.Dtos
+{
+    public class UserForRegisterDto:IDto
+    {
+        public class UserForLoginDto : IDto
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
